95208c7b95761b0198622abeb8f301946a4f153d
AmudanThangavel <amudanthangavel@gmail.com>
2023-08-19 23:47:42 +0530

    Added the base files

---
+++ index.js
@@ -0,0 +1,11 @@
+const buttonRight = document.getElementById("slideRight");
+const buttonLeft = document.getElementById("slideLeft");
+
+buttonRight.onclick = function () {
+  document.getElementById("container").scrollLeft += 600;
+};
+buttonLeft.onclick = function () {
+  document.getElementById("container").scrollLeft -= 600;
+};
+
+
